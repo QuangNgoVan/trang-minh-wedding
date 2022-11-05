23e4b5e455ab7363065777a5aaf91398c1406e4c
QuangNgoVan <quangngovan98@gmail.com>
2022-11-05 11:10:48 +0700

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# my-wedding
+
+My wedding invitation
+
+[https://trang-minh.wedding.vercel.app/](https://trang-minh.wedding.vercel.app/)
---
+++ src/config/index.js
@@ -0,0 +1,11 @@
+export const anHoiLocation = 'https://goo.gl/maps/mKzGDwWms4wdCZor7';
+
+export const tiecCuoiLocation = 'https://goo.gl/maps/mKzGDwWms4wdCZor7';
+
+export const thanhHonLocation = 'https://goo.gl/maps/mKzGDwWms4wdCZor7';
+
+export const facebookCoDau = 'https://www.facebook.com/kenry.trang.9';
+
+export const facebookChuRe = 'https://www.facebook.com/minhtranngocq';
+
+export const rsvpFormUrl = '';
